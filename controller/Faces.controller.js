b9e569beb08c350be24ef2608ed988e1a6b50195
Danilo Novaes <danilo.novaes@live.com>
2018-05-09 20:02:30 -0300

    Birthday v1.1

--- controller/Faces.controller.js
+++ controller/Faces.controller.js
@@ -9,32 +9,66 @@ sap.ui.define([
         return Controller.extend("ModelUI5.controller.Faces", {
 
             onInit: function (evt) {
-                var sDataPath = jQuery.sap.getModulePath("ModelUI5.model.data", "/Birthdays.json");
-                var oModel = new JSONModel(sDataPath);
+
+                var oModel = new JSONModel(jQuery.sap.getModulePath("ModelUI5.model.data", "/Birthdays.json"));
+
+                this._changeframeType(oModel);
+
+
+                // var that = this;
+                // oModel.attachRequestCompleted(function () {
+                //     that._changeframeType(oModel);
+                //     console.log("loaded");
+                // });
+
                 this.getView().setModel(oModel, "birthdays");
-
-
-
-                // var birthdays = this.getView().getModel("birthdays").getData().Birthdays;
-                // var currentdate = new Date();
-
-                // for (var i = 0; i < birthdays.length; i++) {
-
-                // if (birthdays[i].date === currentdate) {
-                //     birthdays[i].isBirthday = "OneByOne";
-                // } else {
-                //     birthdays[i].isBirthday = "TwoByOne";
-                // }
-                // }
-                // oJson.setData(birthdays);
-                // this.getView().setModel(oJson, "produtoSelecionado");
 
             },
 
             onBeforeRendering: function () {},
             onAfterRendering: function () {},
             onExit: function () {},
-            onPress: function () {}
+            onPress: function () {},
+
+            _changeframeType: function (oModel) {
+
+                var currentDate = this._getCurrentDate();
+
+                var birthdays = oModel.getProperty("/Birthdays");
+
+                for (var i = 0; i < birthdays.length; i++) {
+
+                    if (birthdays[i].date.substring(0, 5) == currentDate.substring(0, 5)) {
+                        birthdays[i].isBirthday = "TwoByOne";
+                    } else {
+                        birthdays[i].isBirthday = "OneByOne";
+                    }
+
+                }
+            },
+
+            _getCurrentDate: function () {
+                var q = new Date();
+                var m = q.getMonth() + 1;
+                var d = q.getDate();
+                var y = q.getFullYear();
+
+                d = this._addZero(d);
+                m = this._addZero(m);
+
+                var date = d + '/' + m + '/' + y;
+
+                return date;
+            },
+
+            _addZero: function (date) {
+
+                if (date < 10) {
+                    return date = '0' + date;
+                }
+            }
+
+
         });
 
     });
